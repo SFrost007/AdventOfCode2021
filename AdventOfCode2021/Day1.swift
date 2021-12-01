a976d109fa08ddc03d9e359b5dd82cd7418aaf73
Simon Frost <sfrost007@gmail.com>
2021-12-01 05:08:59 +0000

    Day1: Template output

---
+++ AdventOfCode2021/Day1.swift
@@ -0,0 +1,36 @@
+//
+//  Day1.swift
+//  AdventOfCode2021
+//
+//  Created by Simon Frost on 1/12/2021.
+//
+
+import Foundation
+
+class Day1 {
+    
+    // MARK: - Initialisation
+    
+    let inputData: [String]
+    
+    init(inputURL: URL) {
+        inputData = try! String(contentsOf: inputURL).components(separatedBy: .newlines)
+    }
+    
+    // MARK: - Problem cases
+    
+    func part1() -> Int {
+        fatalError("Not yet implemented")
+    }
+    
+    func part2() -> Int {
+        fatalError("Not yet implemented")
+    }
+    
+    // MARK: - Worker functions
+    
+    static func findSomething(in input: String) -> Int {
+        return -1
+    }
+    
+}
---
+++ AdventOfCode2021Tests/Day1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Day1Tests.swift
+//  AdventOfCode2021Tests
+//
+//  Created by Simon Frost on 1/12/2021.
+//
+
+import XCTest
+@testable import AdventOfCode2021
+
+class Day1Tests: AoCTestCase {
+    
+    let example = Day1(inputURL: urlForExampleData(day: 1))
+    let myInput = Day1(inputURL: urlForMyInputData(day: 1))
+    
+    func testPart1() {
+        XCTAssertEqual(Day1.findSomething(in: example.inputData[0]), 123)
+        XCTAssertEqual(example.part1(), 123)
+        
+        print("Answer for Day 1 Part 1: \(myInput.part1())")
+        measure {
+            let _ = myInput.part1()
+        }
+    }
+    
+    func testPart2() {
+        XCTAssertEqual(Day1.findSomething(in: example.inputData[0]), 123)
+        XCTAssertEqual(example.part2(), 123)
+        
+        print("Answer for Day 1 Part 2: \(myInput.part2())")
+        measure {
+            let _ = myInput.part2()
+        }
+    }
+
+}
