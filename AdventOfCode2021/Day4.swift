88331b2f2190f13150e489b6c063895380e69753
Simon Frost <sfrost007@gmail.com>
2021-12-04 09:00:32 +0000

    Day4: Part1: Add input parsing

--- AdventOfCode2021/Day4.swift
+++ AdventOfCode2021/Day4.swift
@@ -4,20 +4,53 @@ class Day4 {
     
     // MARK: - Initialisation
     
-    let inputData: [String]
+    let calledNumbers: [Int]
+    let bingoBoards: [BingoBoard]
     
     init(inputURL: URL) {
-        inputData = try! String(contentsOf: inputURL).components(separatedBy: .newlines)
+        let inputData = try! String(contentsOf: inputURL)
+        let firstLine = inputData.components(separatedBy: .newlines).first!
+        calledNumbers = firstLine.components(separatedBy: ",").compactMap { Int($0) }
+        
+        let inputWithoutCalledNumbers = inputData.components(separatedBy: .newlines)[2...].joined(separator: "\n")
+        let boardsData = inputWithoutCalledNumbers.components(separatedBy: "\n\n")
+        bingoBoards = boardsData.map { BingoBoard($0) }
+    }
+    
+    // MARK: = Data structures
+    
+    struct BingoBoard {
+        struct Cell {
+            var number: Int
+            var called: Bool
+        }
+        
+        var numberGrid: [[Cell]]
+        
+        init(_ string: String) {
+            numberGrid = string
+                .components(separatedBy: .newlines)
+                .filter { !$0.isEmpty }
+                .map {
+                    $0.components(separatedBy: .whitespaces)
+                        .compactMap { Int($0) }
+                        .compactMap { Cell(number: $0, called: false) }
+                }
+        }
+        
+        func isCompleted() -> Bool {
+            return false
+        }
     }
     
     // MARK: - Problem cases
     
     func part1() -> Int {
-        fatalError("Not yet implemented")
+        return -1
     }
     
     func part2() -> Int {
-        fatalError("Not yet implemented")
+        return -1
     }
     
 }
--- AdventOfCode2021Tests/Day4Tests.swift
+++ AdventOfCode2021Tests/Day4Tests.swift
@@ -7,6 +7,12 @@ class Day4Tests: AoCTestCase {
     let myInput = Day4(inputURL: urlForMyInputData(day: 4))
     
     func testPart1() {
+        // Parsing tests
+        XCTAssertEqual(example.calledNumbers, [7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1])
+        XCTAssertEqual(example.bingoBoards.count, 3)
+        XCTAssertEqual(example.bingoBoards.first!.numberGrid.first!.map { $0.number }, [22,13,17,11,0])
+        XCTAssertEqual(example.bingoBoards.last!.numberGrid.last!.map { $0.number }, [2,0,12,3,7])
+        // Algorithm tests
         XCTAssertEqual(example.part1(), 4512)
         
         printAnswer(day: 4, part: 1, answer: myInput.part1())
