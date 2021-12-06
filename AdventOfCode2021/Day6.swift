2a677d58f0da380b6021f13a31a09fd4f86723fa
Simon Frost <sfrost007@gmail.com>
2021-12-06 15:41:59 +0000

    Day6: Part2: Working efficient solution

--- AdventOfCode2021/Day6.swift
+++ AdventOfCode2021/Day6.swift
@@ -25,7 +25,36 @@ class Day6 {
     }
     
     func part2() -> Int {
-        return -1
+        Self.wipeCache()
+        // Normalise the fish's initial ages to a date they were born on
+        let birthDates = inputData.map { $0 - 8 } // Example: [3,4,3,1,2] -> [-5,-4,-5,-7,-8]
+        return birthDates.map { Self.cumulativeFishCreatedUpToCutoff(fromFishBornOnDay: $0) }.reduce(0, +)
+    }
+    
+    // MARK: - Cached calculation function
+    
+    // Keep a cache for the 'cumulative' function as any fish born on the same day will produce the same number of children.
+    // To make this more dynamic it could be a multi-layer cache with an extra key for the number of days we're calculating
+    // up to, but for the puzzle we only ever need it up to 256.
+    static let dayCutoff = 256
+    static var cache: [Int: Int] = [:]
+    
+    static func wipeCache() {
+        cache = [:]
+    }
+    
+    static func cumulativeFishCreatedUpToCutoff(fromFishBornOnDay day: Int) -> Int {
+        guard day <= dayCutoff else { fatalError("") }
+        if let previousCalculatedValue = cache[day] { return previousCalculatedValue }
+        
+        var fishCounter = 1 // For self
+        var reproduceCycle = day + 2 // Fish are born with a counter of 8, but reproduce cycle resets to 6
+        while reproduceCycle + 7 <= dayCutoff {
+            reproduceCycle += 7
+            fishCounter += cumulativeFishCreatedUpToCutoff(fromFishBornOnDay: reproduceCycle)
+        }
+        cache[day] = fishCounter
+        return fishCounter
     }
     
 }
--- AdventOfCode2021Tests/Day6Tests.swift
+++ AdventOfCode2021Tests/Day6Tests.swift
@@ -15,10 +15,32 @@ class Day6Tests: AoCTestCase {
     }
     
     func testPart2() {
-        XCTAssertEqual(example.part2(), 26984457539)
+        // Test individual days up to 256
+        XCTAssertEqual(Day6.cumulativeFishCreatedUpToCutoff(fromFishBornOnDay: 256), 1) // Will never reproduce
+        XCTAssertEqual(Day6.cumulativeFishCreatedUpToCutoff(fromFishBornOnDay: 255), 1) // Will never reproduce
+        XCTAssertEqual(Day6.cumulativeFishCreatedUpToCutoff(fromFishBornOnDay: 254), 1) // Will never reproduce
+        XCTAssertEqual(Day6.cumulativeFishCreatedUpToCutoff(fromFishBornOnDay: 253), 1) // Will never reproduce
+        XCTAssertEqual(Day6.cumulativeFishCreatedUpToCutoff(fromFishBornOnDay: 252), 1) // Will never reproduce
+        XCTAssertEqual(Day6.cumulativeFishCreatedUpToCutoff(fromFishBornOnDay: 251), 1) // Will never reproduce
+        XCTAssertEqual(Day6.cumulativeFishCreatedUpToCutoff(fromFishBornOnDay: 250), 1) // Will never reproduce
+        XCTAssertEqual(Day6.cumulativeFishCreatedUpToCutoff(fromFishBornOnDay: 249), 1) // Will never reproduce
+        XCTAssertEqual(Day6.cumulativeFishCreatedUpToCutoff(fromFishBornOnDay: 248), 1) // Will never reproduce
+        XCTAssertEqual(Day6.cumulativeFishCreatedUpToCutoff(fromFishBornOnDay: 247), 2) // Will repro on 256 (+9)
+        // Skip some repeated `2`s
+        XCTAssertEqual(Day6.cumulativeFishCreatedUpToCutoff(fromFishBornOnDay: 241), 2) // Will repro on 250 (+9)
+        XCTAssertEqual(Day6.cumulativeFishCreatedUpToCutoff(fromFishBornOnDay: 240), 3) // Will repro on 249 (+9) and 256 (+7)
+        XCTAssertEqual(Day6.cumulativeFishCreatedUpToCutoff(fromFishBornOnDay: 239), 3) // Will repro on 248 (+9) and 255 (+7)
+        XCTAssertEqual(Day6.cumulativeFishCreatedUpToCutoff(fromFishBornOnDay: 238), 4) // Will repro on 247 (+9) and 254 (+7), plus child from 247 will repro once
+        XCTAssertEqual(Day6.cumulativeFishCreatedUpToCutoff(fromFishBornOnDay: 231), 7) // Will repro on 240 (+9), 247 (+7) and 254 (+7), = self +3 from 240, +2 from 247, +1 from 254
+        // Test the same again with a clear cache
+        Day6.wipeCache()
+        XCTAssertEqual(Day6.cumulativeFishCreatedUpToCutoff(fromFishBornOnDay: 231), 7)
         
+        // Test full calculation
+        XCTAssertEqual(example.part2(), 26984457539)
+
         printAnswer(day: 6, part: 2, answer: myInput.part2())
-        measure { XCTAssertEqual(myInput.part2(), -1) }
+        measure { XCTAssertEqual(myInput.part2(), 1693022481538) }
     }
 
 }
