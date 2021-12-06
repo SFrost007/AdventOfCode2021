a0fdc694f96e573728e143b7604435409399124e
Simon Frost <sfrost007@gmail.com>
2021-12-06 08:28:42 +0000

    Day6: Part1: Working solution

--- AdventOfCode2021/Day6.swift
+++ AdventOfCode2021/Day6.swift
@@ -4,16 +4,22 @@ class Day6 {
     
     // MARK: - Initialisation
     
-    let inputData: [String]
+    let inputData: [Int]
     
     init(inputURL: URL) {
-        inputData = try! String(contentsOf: inputURL).components(separatedBy: .newlines)
+        inputData = try! String(contentsOf: inputURL).trimmingCharacters(in: .newlines)
+            .components(separatedBy: ",")
+            .compactMap { Int($0) }
     }
     
     // MARK: - Problem cases
     
     func part1() -> Int {
-        fatalError("Not yet implemented")
+        var fishAges = inputData
+        for _ in 1...80 {
+            fishAges = Self.processDay(fishAges)
+        }
+        return fishAges.count
     }
     
     func part2() -> Int {
@@ -22,8 +28,9 @@ class Day6 {
     
     // MARK: - Worker functions
     
-    static func findSomething(in input: String) -> Int {
-        return -1
+    // Note: This doesn't append new fish to the end like the example, but order doesn't matter for part 1
+    static func processDay(_ fishAges: [Int]) -> [Int] {
+        return fishAges.flatMap { $0 > 0 ? [($0 - 1)] : [6, 8] }
     }
     
 }
--- AdventOfCode2021Tests/Day6Tests.swift
+++ AdventOfCode2021Tests/Day6Tests.swift
@@ -7,15 +7,14 @@ class Day6Tests: AoCTestCase {
     let myInput = Day6(inputURL: urlForMyInputData(day: 6))
     
     func testPart1() {
-        XCTAssertEqual(Day6.findSomething(in: example.inputData[0]), 123)
-        XCTAssertEqual(example.part1(), 123)
+        XCTAssertEqual(example.inputData, [3,4,3,1,2]) // Check parsing
+        XCTAssertEqual(example.part1(), 5934)
         
         printAnswer(day: 6, part: 1, answer: myInput.part1())
-        measure { XCTAssertEqual(myInput.part1(), -1) }
+        measure { XCTAssertEqual(myInput.part1(), 376194) }
     }
     
     func testPart2() {
-        XCTAssertEqual(Day6.findSomething(in: example.inputData[0]), 123)
         XCTAssertEqual(example.part2(), 123)
         
         printAnswer(day: 6, part: 2, answer: myInput.part2())
