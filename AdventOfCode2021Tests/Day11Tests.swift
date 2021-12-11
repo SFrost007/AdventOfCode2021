e588e9a0b5423a5fb223cb37adc83ef5440c6bcd
Simon Frost <sfrost007@gmail.com>
2021-12-11 15:13:21 +0000

    Day11: Add template

---
+++ AdventOfCode2021Tests/Day11Tests.swift
@@ -0,0 +1,23 @@
+import XCTest
+@testable import AdventOfCode2021
+
+class Day11Tests: AoCTestCase {
+    
+    let example = Day11(inputURL: urlForExampleData(day: 11))
+    let myInput = Day11(inputURL: urlForMyInputData(day: 11))
+    
+    func testPart1() {
+        XCTAssertEqual(example.part1(), 1656)
+        
+        printAnswer(day: 11, part: 1, answer: myInput.part1())
+        measure { XCTAssertEqual(myInput.part1(), -1) }
+    }
+    
+    func testPart2() {
+        XCTAssertEqual(example.part2(), 123)
+        
+        printAnswer(day: 11, part: 2, answer: myInput.part2())
+        measure { XCTAssertEqual(myInput.part2(), -1) }
+    }
+
+}
