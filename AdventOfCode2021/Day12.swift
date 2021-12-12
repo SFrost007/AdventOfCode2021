f35e5d9016219fe0caf31bea98c88eb85825d74b
Simon Frost <sfrost007@gmail.com>
2021-12-12 16:40:52 +0000

    Day12: Part1: Working solution

--- AdventOfCode2021/Day12.swift
+++ AdventOfCode2021/Day12.swift
@@ -4,20 +4,65 @@ class Day12 {
     
     // MARK: - Initialisation
     
-    let inputData: [String]
+    let nodeLookup: [String: Node]
     
     init(inputURL: URL) {
-        inputData = try! String(contentsOf: inputURL).components(separatedBy: .newlines)
+        var nodeCache: [String: Node] = [:]
+        try! String(contentsOf: inputURL)
+            .components(separatedBy: .newlines)
+            .filter { !$0.isEmpty }
+            .forEach {
+                let nodes = $0
+                    .components(separatedBy: "-")
+                    .map { nodeCache[$0] ?? Node(name: $0) }
+                nodes.first!.addConnection(to: nodes.last!)
+                nodes.forEach { nodeCache[$0.name] = $0 }
+            }
+        nodeLookup = nodeCache
+    }
+    
+    // MARK: - Data Structure
+    
+    class Node: Equatable {
+        let name: String
+        var connections: [Node] = []
+        
+        init(name: String) {
+            self.name = name
+        }
+        
+        func addConnection(to node: Node) {
+            guard !connections.contains(node) else { return }
+            // Establish two-way link
+            connections.append(node)
+            node.connections.append(self)
+        }
+        
+        var isUppercase: Bool { name.uppercased() == name }
+        
+        static func == (lhs: Day12.Node, rhs: Day12.Node) -> Bool {
+            return lhs.name == rhs.name
+        }
     }
     
     // MARK: - Problem cases
     
     func part1() -> Int {
-        fatalError("Not yet implemented")
+        Self.pathsFromVisiting(node: nodeLookup["start"]!, in: nodeLookup, visited: [])
+            .filter { $0.last == "end" }
+            .count
     }
     
     func part2() -> Int {
         fatalError("Not yet implemented")
+    }
+    
+    static func pathsFromVisiting(node: Node, in lookup: [String: Node], visited: [String]) -> [[String]] {
+        guard node.isUppercase || !visited.contains(node.name) else { return [visited] }
+        guard node.name != "end" else { return [visited + ["end"]] }
+        return node.connections.flatMap {
+            pathsFromVisiting(node: $0, in: lookup, visited: visited + [node.name])
+        }
     }
     
 }
--- AdventOfCode2021Tests/Day12Tests.swift
+++ AdventOfCode2021Tests/Day12Tests.swift
@@ -8,13 +8,23 @@ class Day12Tests: AoCTestCase {
     let example3 = Day12(inputURL: urlForExampleData(day: 12, partSuffix: "1c"))
     let myInput = Day12(inputURL: urlForMyInputData(day: 12))
     
+    func testParsing() {
+        XCTAssertEqual(example1.nodeLookup.count, 6)
+        XCTAssertEqual(example1.nodeLookup["start"]!.connections.count, 2)
+        XCTAssertEqual(example1.nodeLookup["A"]!.connections.count, 4)
+        XCTAssertEqual(example1.nodeLookup["b"]!.connections.count, 4)
+        XCTAssertEqual(example1.nodeLookup["c"]!.connections.count, 1)
+        XCTAssertEqual(example1.nodeLookup["d"]!.connections.count, 1)
+        XCTAssertEqual(example1.nodeLookup["end"]!.connections.count, 2)
+    }
+    
     func testPart1() {
         XCTAssertEqual(example1.part1(), 10)
         XCTAssertEqual(example2.part1(), 19)
         XCTAssertEqual(example3.part1(), 226)
         
         printAnswer(day: 12, part: 1, answer: myInput.part1())
-        measure { XCTAssertEqual(myInput.part1(), -1) }
+        measure { XCTAssertEqual(myInput.part1(), 3410) }
     }
     
     func testPart2() {
