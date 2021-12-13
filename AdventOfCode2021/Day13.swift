97b0482e1c41c4dde7c5773863373810723cf36d
Simon Frost <sfrost007@gmail.com>
2021-12-13 05:43:08 +0000

    Day13: Part1: Working solution

--- AdventOfCode2021/Day13.swift
+++ AdventOfCode2021/Day13.swift
@@ -4,20 +4,65 @@ class Day13 {
     
     // MARK: - Initialisation
     
-    let inputData: [String]
+    struct Dot: Hashable {
+        let x: Int
+        let y: Int
+    }
+    
+    struct FoldInstruction {
+        enum Axis: String {
+            case x, y
+        }
+        let axis: Axis
+        let value: Int
+    }
+    
+    let dots: [Dot]
+    let instructions: [FoldInstruction]
     
     init(inputURL: URL) {
-        inputData = try! String(contentsOf: inputURL).components(separatedBy: .newlines)
+        let inputData = try! String(contentsOf: inputURL)
+            .components(separatedBy: "\n\n")
+        dots = inputData
+            .first!
+            .components(separatedBy: .newlines)
+            .filter { !$0.isEmpty }
+            .map { $0.components(separatedBy: ",") }
+            .map { Dot(x: Int($0.first!)!, y: Int($0.last!)!) }
+        instructions = inputData
+            .last!
+            .components(separatedBy: .newlines)
+            .filter { !$0.isEmpty }
+            .map { $0.replacingOccurrences(of: "fold along ", with: "") }
+            .map { $0.components(separatedBy: "=") }
+            .map { FoldInstruction(axis: FoldInstruction.Axis(rawValue: $0.first!)!, value: Int($0.last!)!) }
     }
     
     // MARK: - Problem cases
     
     func part1() -> Int {
-        fatalError("Not yet implemented")
+        Set(Self.getNewDots(from: dots, after: instructions.first!)).count
     }
     
     func part2() -> Int {
         fatalError("Not yet implemented")
+    }
+    
+    // MARK: - Helper functions
+    
+    static func getNewDots(from dots: [Dot], after instruction: FoldInstruction) -> [Dot] {
+        dots.map { (dot) -> Dot in
+            switch instruction.axis {
+            case .x:
+                guard dot.x > instruction.value else { return dot }
+                let newValue = instruction.value - (dot.x - instruction.value)
+                return Dot(x: newValue, y: dot.y)
+            case .y:
+                guard dot.y > instruction.value else { return dot }
+                let newValue = instruction.value - (dot.y - instruction.value)
+                return Dot(x: dot.x, y: newValue)
+            }
+        }
     }
     
 }
--- AdventOfCode2021Tests/Day13Tests.swift
+++ AdventOfCode2021Tests/Day13Tests.swift
@@ -6,11 +6,16 @@ class Day13Tests: AoCTestCase {
     let example = Day13(inputURL: urlForExampleData(day: 13))
     let myInput = Day13(inputURL: urlForMyInputData(day: 13))
     
+    func testParsing() {
+        XCTAssertEqual(example.dots.count, 18)
+        XCTAssertEqual(example.instructions.count, 2)
+    }
+    
     func testPart1() {
         XCTAssertEqual(example.part1(), 17)
         
         printAnswer(day: 13, part: 1, answer: myInput.part1())
-        measure { XCTAssertEqual(myInput.part1(), -1) }
+        measure { XCTAssertEqual(myInput.part1(), 827) }
     }
     
     func testPart2() {
